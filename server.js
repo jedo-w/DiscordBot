94bf3f9167f0cf2dbf74ee6c0c8c9f18af489f34
jedo-wood <68785573+jedo-w@users.noreply.github.com>
2021-06-23 05:27:51 +0900

    Create server.js
    discord.js bot

---
+++ server.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const querystring = require('querystring');
+const discord = require('discord.js');
+const client = new discord.Client();
+
+http.createServer(function(req, res){
+ if (req.method == 'POST'){
+   var data = "";
+   req.on('data', function(chunk){
+     data += chunk;
+   });
+   req.on('end', function(){
+     if(!data){
+        console.log("No post data");
+        res.end();
+        return;
+     }
+     var dataObject = querystring.parse(data);
+     console.log("post:" + dataObject.type);
+     if(dataObject.type == "wake"){
+       console.log("Woke up in post");
+       res.end();
+       return;
+     }
+     res.end();
+   });
+ }
+ else if (req.method == 'GET'){
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Discord Bot is active now\n');
+ }
+}).listen(3000);
+
+client.on('ready', ()=>{
+ console.log(`Bot ready down! ${client.user.tag}!`);
+ client.user.setPresence({ activity: { name: 'げーむ' } });
+});
+
+client.on('message', msg =>{
+ if (msg.author.id == client.user.id){
+   return;
+ }
+ if(msg.isMemberMentioned(client.user)){
+   sendReply(msg, "呼びましたか？");
+   return;
+ }
+ if (msg.content.match(/にゃ～ん|にゃーん|hi/)){
+   let text = "にゃ～ん";
+   sendMsg(msg.channel.id, text);
+   return;
+ }
+if (msg.content === 'ping'){
+  msg.reply('pong!');
+   //write msg name & message & time to google spreed sheet
+   return;
+ }
+if (msg.content == 'kick') {
+    
+  msg.reply('why did you kick me!');
+  //msg.channels.get(8xxxxx4).send('猫です')
+ }
+  
+});
+
+if(process.env.DISCORD_BOT_TOKEN == undefined){
+console.log('DISCORD_BOT_TOKENが設定されていません。');
+process.exit(0);
+}
+
+client.login( process.env.DISCORD_BOT_TOKEN );
+
+function sendReply(message, text){
+ message.reply(text)
+   .then(console.log("リプライ送信: " + text))
+   .catch(console.error);
+}
+
+function sendMsg(channelId, text, option={}){
+ client.channels.get(channelId).send(text, option)
+   .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
+   .catch(console.error);
+}
